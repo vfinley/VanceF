12d9aa82445f878a688a90eccceb90c5956d5a79
Vance Finley <vfinley@csil-23.cs.ucsb.edu>
2020-01-08 15:56:32 -0800

    iniy

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+// hello.cpp
+// Vance Finley
+// 981068-0
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout<<"Hello World!" <<endl;
+	cout<<"I am ready for CS16!" <<endl;
+
+
+
+	return 0;
+}
